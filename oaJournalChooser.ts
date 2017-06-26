fce62a268081b6102fe54015c50e429b4c2f2e46
mrittman <smart3141@netscape.net>
2017-06-26 17:26:49 +0200

    First commit with workable flowchart

---
+++ oaJournalChooser.ts
@@ -0,0 +1,11 @@
+<* Tag suit for choosing an open access journal 
+*>
+
+DataTags: consists of complete, preprint, preprintFeedback, journalKnown, licenses, licenseKnown.
+
+complete [has the paper been written?]: one of yes, no.
+preprint [has a preprint been posted?]: one of yes, no.
+preprintFeedback [did the author get preprint feedback?]: one of yes, no.
+journalKnown [has an OA journal been identified?]: one of oa, hybrid, notOpen, no.
+licenses: one of single, many.
+licenseKnown: one of yes, no.
